d0e5b888e141b408f37b8690b6f0f023572ee3ed
Alfred Rubin <alfredo.rubin@neo4j.com>
2022-06-28 14:55:49 +0200

    changed folder name and trying to wrap debeziumConfig with JSON files

--- configDebeziumConnector.sh
+++ configDebeziumConnector.sh
@@ -1,25 +1,29 @@
 # Sending the connector configuration to Debezium
-curl -i -X PUT -H "Content-Type:application/json" \
-  http://localhost:8083/connectors/source-debezium-orders-00/config \
-  -d '{
-            "connector.class": "io.debezium.connector.mysql.MySqlConnector",
-            "database.hostname": "my-sql",
-            "database.port": "3306",
-            "database.user": "debezium",
-            "database.password": "password",
-            "database.server.id": "42",
-            "database.server.name": "mysql_connector",
-            "table.whitelist": "demo.customers,demo.orders",
-            "database.history.kafka.bootstrap.servers": "broker:9092",
-            "database.history.kafka.topic": "dbhistory.demo" ,
-            "decimal.handling.mode": "double",
-            "include.schema.changes": "true",
-            "transforms": "dropTopicPrefix",
-            "transforms.dropTopicPrefix.type":"org.apache.kafka.connect.transforms.RegexRouter",
-            "transforms.dropTopicPrefix.regex":"mysql_connector.demo.(.*)",
-            "transforms.dropTopicPrefix.replacement":"$1",
-            "key.converter": "io.confluent.connect.avro.AvroConverter",
-            "key.converter.schema.registry.url": "http://schema-registry:8081",
-            "value.converter": "io.confluent.connect.avro.AvroConverter",
-            "value.converter.schema.registry.url": "http://schema-registry:8081"
-    }'
+#curl -i -X PUT -H "Content-Type:application/json" \
+#  http://localhost:8083/connectors/source-debezium-orders-00/config \
+#  -d '{
+#            "connector.class": "io.debezium.connector.mysql.MySqlConnector",
+#            "database.hostname": "my-sql",
+#            "database.port": "3306",
+#            "database.user": "debezium",
+#            "database.password": "password",
+#            "database.server.id": "42",
+#            "database.server.name": "mysql_connector",
+#            "table.whitelist": "demo.customers,demo.orders",
+#            "database.history.kafka.bootstrap.servers": "broker:9092",
+#            "database.history.kafka.topic": "dbhistory.demo" ,
+#            "decimal.handling.mode": "double",
+#            "include.schema.changes": "true",
+#            "transforms": "dropTopicPrefix",
+#            "transforms.dropTopicPrefix.type":"org.apache.kafka.connect.transforms.RegexRouter",
+#            "transforms.dropTopicPrefix.regex":"mysql_connector.demo.(.*)",
+#            "transforms.dropTopicPrefix.replacement":"$1",
+#            "key.converter": "io.confluent.connect.avro.AvroConverter",
+#            "key.converter.schema.registry.url": "http://schema-registry:8081",
+#            "value.converter": "io.confluent.connect.avro.AvroConverter",
+#            "value.converter.schema.registry.url": "http://schema-registry:8081"
+#    }'
+curl -X POST http://localhost:8083/connectors \
+  -H 'Content-Type:application/json' \
+  -H 'Accept:application/json' \
+  -d @kakfaConnectConfigs/debeziumConfig.source.avro.mysql.json
